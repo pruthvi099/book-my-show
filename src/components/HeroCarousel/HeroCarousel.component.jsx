70f73dbac0a8a0b2629785450c37f0e60594f8f6
pruthvi099 <65605220+pruthvi099@users.noreply.github.com>
2021-09-20 08:05:08 +0530

    add Arrow Components

--- src/components/HeroCarousel/HeroCarousel.component.jsx
+++ src/components/HeroCarousel/HeroCarousel.component.jsx
@@ -1,43 +1,72 @@
 import React from "react";
+import HeroSlider from "react-slick";
 
-import HeroSlider from "react-slick"
-
-// Import CSS File
-import "slick-carousel/slick/slick.css"; 
+import { NextArrow,PrevArrow } from "./Arrows.component";
+// Import css files
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
-const HeroCarousel=()=>{
-    const settings={
-        arrows:true,
-        centerMode:true,
-        centerPadding:"350px",
-        dots:true,
-        infinite:true,
-        speed:500,
-        slidesToShow:1,
-        slidesToScroll:1,
-    }
 
-    const images=[
-        "https://images.unsplash.com/photo-1536440136628-849c177e76a1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-        "https://images.unsplash.com/photo-1542204165-65bf26472b9b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-        "https://images.unsplash.com/photo-1601643157091-ce5c665179ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80",
-        "https://images.unsplash.com/photo-1524985069026-dd778a71c7b4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
-    ]
-    return<>
-    <HeroSlider {...settings}>
-        {images.map((images)=>(
-            <div className="w-20 h-80">
-                <img src={images} alt="testing" className="w-full h-full"  />
+const HeroCarousal = () => {
+  const settingsLG = {
+    arrows: true,
+    autoplay: true,
+    centerMode: true,
+    centerPadding: "300px",
+    slidesToShow: 1,
+    infinite: true,
+    slidesToScroll: 1,
+    nextArrow:<NextArrow />,
+    prevArrow:<PrevArrow />,
+  };
+
+  const settings = {
+    arrows: true,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow:<NextArrow />,
+    prevArrow:<PrevArrow />,
+  };
+
+  const images = [
+    "https://images.unsplash.com/photo-1593642531955-b62e17bdaa9c?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+    "https://images.unsplash.com/photo-1626128665085-483747621778?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=610&q=80",
+    "https://images.unsplash.com/photo-1622495894321-010d1e6db2a9?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2775&q=80",
+    "https://images.unsplash.com/photo-1626050954744-92bf034ce476?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
+    "https://images.unsplash.com/photo-1626074961596-cab914d9392e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=664&q=80",
+  ];
+
+  return (
+    <>
+      <div className="lg:hidden">
+        <HeroSlider {...settings}>
+          {images.map((image) => (
+            <div className="w-full h-56 md:h-80 py-3 ">
+              <img src={image} alt="testing" className="w-full h-full" />
             </div>
-        )
+          ))}
+        </HeroSlider>
+      </div>
 
-        )}
-
-    </HeroSlider>
-
+      <div className="hidden lg:block">
+        <HeroSlider {...settingsLG}>
+          {images.map((image) => (
+            <div className="w-full h-96 px-2 py-3">
+              <img
+                src={image}
+                alt="testing"
+                className="w-full h-full rounded-md"
+              />
+            </div>
+          ))}
+        </HeroSlider>
+      </div>
     </>
-}
+  );
+};
 
-export default HeroCarousel;
+export default HeroCarousal;
